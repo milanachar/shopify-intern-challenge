c3133b5354a16ee69e09da229f0b5bf22767d2de
Milan Ganesh Acharya <ml650738@dal.ca>
2022-05-22 18:03:07 -0300

    Added comments

--- src/App.js
+++ src/App.js
@@ -2,22 +2,29 @@ import { useEffect, useState } from 'react';
 import { Container, Row, Form, Button, Table } from 'react-bootstrap';
 
 function App() {
+  // Holds the prompt entered in the textarea
   const [prompt, setPrompt] = useState('');
+  // Holds the responses received from the AI engine. Initialized using the value present in the local storage
   const [responses, setResponses] = useState(localStorage.getItem('response') ? JSON.parse(localStorage.getItem('response')) : []);
+  // Holds the AI engine to use
   const [engine, setEngine] = useState('text-curie-001');
 
+  // Store the value in local storage as the responses are updated
   useEffect(() => {
     localStorage.setItem('response', JSON.stringify(responses));
   }, [responses]);
 
+  // Handle the form submit
   const handleSubmit = event => {
     event.preventDefault();
 
+    // Parameters for the POST request
     const params = {
       prompt: prompt,
       max_tokens: 64
     };
 
+    // Send the request
     fetch(`https://api.openai.com/v1/engines/${engine}/completions`, {
       method: "POST",
       headers: {
@@ -28,6 +35,7 @@ function App() {
     }).then(response => {
       return response.json();
     }).then(jsonResponse => {
+      // Append response at the front of the JSON object array
       setResponses([{ prompt: prompt, response: jsonResponse.choices[0].text, id:jsonResponse.id }, ...responses]);
     }).catch(error => {
       console.log(error);
@@ -48,14 +56,14 @@ function App() {
             <Form.Group className="mb-3">
               <Form.Label style={{ fontWeight: 'bold' }}>AI Engine</Form.Label>
               <Form.Select onChange={(e) => setEngine(e.target.value)}>
-                <option value="text-curie-001">Curie</option>
+                <option value="text-curie-001">Curie (Default)</option>
                 <option value="text-davinci-002">Da Vinci</option>
                 <option value="text-babbage-001">Babbage</option>
                 <option value="text-ada-001">Ada</option>
               </Form.Select>
             </Form.Group>
 
-            <Button variant="primary" type="submit">
+            <Button variant="dark" type="submit">
               Submit
             </Button>
           </Form>
